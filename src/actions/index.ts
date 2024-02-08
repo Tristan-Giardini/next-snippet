38b22d9250c4882c1b9b1b49c2ca39e5e1218637
Tristan Giardini <tristangiardini@gmail.com>
2024-02-08 16:49:18 -0500

    Section 5 complete

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,43 @@ export async function deleteSnippet(id: number) {
 
     redirect('/')
 }
+
+export async function createSnippet(formState: {message: string}, formData: FormData) {
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    try {
+
+if (typeof title !== 'string' || title.length < 3) {
+return {
+    message: 'Title must be longer'
+}
+}
+
+if (typeof code !== 'string' || code.length < 10) {
+return {
+        message: 'Code must be longer'
+    }
+}
+
+    await db.snippet.create({
+        data: {
+            title: title,
+            code: code
+        }
+    })
+
+} catch (err: unknown) {
+    if (err instanceof Error) {
+        return {
+            message: err.message
+        }
+    } else {
+        return { 
+            message: 'Something went wrong...'
+    };
+    }
+}
+
+    redirect('/')
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,32 +1,13 @@
-import { db } from '@/db'
-import { redirect } from 'next/navigation';
+'use client'
+
+import { useFormState } from "react-dom"
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-    async function createSnippet(formData: FormData) {
-        // This needs to be a server action
-        'use server'
-
-        // Check the user's inputs and make sure they're valid
-        const title = formData.get('title') as string;
-        const code = formData.get('code') as string;
-
-        // Create a new record in the database
-        const snippet = await db.snippet.create({
-            data: {
-                title: title,
-                code: code
-            }
-        })
-
-        console.log(snippet);
-
-        // Bring them back to homepage
-        redirect('/')
-
-    }
+const [formState, action] = useFormState(actions.createSnippet, { message: "" })
 
     return (
-        <form action={createSnippet}>
+        <form action={action}>
             <h3 className="font-bold m-3">
                 Create a snippet
             </h3>
@@ -49,6 +30,13 @@ export default function SnippetCreatePage() {
                     id="code"
                     />
                 </div>
+
+                {
+                    formState.message ? (
+                    <div className="my-2 p-2 bg-red-200">{formState.message}</div>
+                    )
+                    : null
+                }
 
                 <button type="submit" className="rounded p-2 bg-blue-200">Create</button>
             </div>
